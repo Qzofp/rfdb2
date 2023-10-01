116ec7f1900e8ad020a5f6ae35e758176757966d
qzofp <qzofp@xbuild>
2023-10-01 11:49:58 +0200

    Design fase: Added demo test pages.

--- public_html/js/stats.js
+++ public_html/js/stats.js
@@ -7,7 +7,7 @@
  * Dependenties: js/chart.js
  *
  * Created on Sep 24, 2023
- * Updated on Sep 24, 2023
+ * Updated on Sep 25, 2023
  *
  * Description: The stats js page which generates the charts.
 
@@ -18,7 +18,7 @@
  * Function:    dummyBarChart
  *
  * Created on Sep 24, 2023
- * Updated on Sep 24, 2023
+ * Updated on Sep 25, 2023
  *
  * Description: Create and show a dummy bar chart met dummy data.
  *
@@ -27,12 +27,12 @@
  *
  */
 function dummyBarChart() { 
-  const ctx = document.getElementById('bar_chart');  
- 
+
   labels = 'Januari,Februari,Maart,April,Mei,Juni,Juli,Augustus,September,Oktober,November,December'; 
   outcome = '3121.07,4484.07,2584.46,2140.08,3533,3330.94,3428.56,2729.38,4557.77,2511.26,6289.92,5596.15';
   income  = '3333.15,3282.51,3380.93,3360.01,5713.57,3406.21,3472.71,3290.01,3731.51,3365.83,5963.46,7480.84';       
-    
+
+  const ctx = document.getElementById('bar_chart');          
   new Chart(ctx, {
     type: 'bar',
     data: {
@@ -84,8 +84,66 @@ function dummyBarChart() {
                        return label;
                     }
                 }
-            }
-        }   
+            },
+            title: {
+                display: true,
+                text: 'Dummy Bar Chart'
+            }            
+        }
     }
   });
+}
+
+/*
+ * Function:    dummyPieChart
+ *
+ * Created on Sep 25, 2023
+ * Updated on Sep 25, 2023
+ *
+ * Description: Create and show a dummy bar chart met dummy data.
+ *
+ * In: -
+ * Out: -
+ *
+ */
+function dummyPieChart() { 
+  const ctx = document.getElementById('pie_chart');  
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ['Financiën','Beleggen','Sparen','Crypto'],
+      datasets: [
+      {
+        label: 'Waarde',
+        data: ['38752.64','75885.52','50950.50','143987.06']
+      }
+     ]
+    },
+    options: {
+      //locale: 'nl-NL',
+      maintainAspectRatio: false,
+      plugins: {
+        tooltip: {
+             callbacks: {
+                label: function(context) {
+                       let label = context.dataset.label || '';
+
+                       if (label) {
+                           label += ': ';
+                       }
+                       if (context.parsed !== null) {
+                           label += new Intl.NumberFormat('nl-NL', { style: 'currency', currency: 'EUR' }).format(context.parsed);
+                       }
+                                            
+                       return label;
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: 'Dummy Pie Chart'
+            }            
+        }
+    }
+  });   
 }
