e23b8f9cbfb74110cca7a1b21060c1db92c71427
qzofp <qzofp@xbuild>
2023-10-02 15:34:45 +0200

    Started with JS.

---
+++ public_html/js/config.js
@@ -0,0 +1,27 @@
+/*
+ * Title: Rizzo's Finances Database
+ * Author: Rizzo Productions
+ * Version: 0.1
+ *
+ * File:    config.js
+ * Used in: sheet.js
+ *
+ * Created on Oct 02, 2023
+ * Updated on Oct 02, 2023
+ *
+ * Description: .
+ * Dependenties: -
+ *
+ */
+
+// Define constants
+$test = "This is a constant!";
+const cTest = $test;
+
+
+
+function showConfigs() {
+    
+    console.log("This is configs.js");
+    
+}
---
+++ public_html/js/sheet.js
@@ -0,0 +1,40 @@
+/*
+ * Title: Rizzo's Finances Database
+ * Author: Rizzo Productions
+ * Version: 0.1
+ *
+ * File:    sheet.js
+ * Used in: sheet.html
+ *
+ * Created on Oct 02, 2023
+ * Updated on Oct 02, 2023
+ *
+ * Description: .
+ * Dependenties: js/config.js
+ *
+ */
+
+//////////////////////////////////////////      Main Functions     ///////////////////////////////////////////
+
+/*
+ * Function:    loadSheet
+ *
+ * Created on Oct 02, 2023
+ * Updated on Oct 02, 2023
+ *
+ * Description: The sheet.html main function.
+ *
+ * In:  -
+ * Out: -
+ *
+ */
+function loadSheet() {
+    
+    
+    // loadConfigs();
+    showConfigs();
+    
+    console.log(cTest);
+    
+}
+
