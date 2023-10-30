830bac6bb72333840c65805ac727d6be80fc6c8d
qzofp <qzofp@xbuild>
2023-10-30 15:12:45 +0100

    Started with the framework.

--- public_html/js/sheet.js
+++ public_html/js/sheet.js
@@ -6,8 +6,8 @@
  * File:    sheet.js
  * Used in: sheet.html
  *
- * Created on Oct 02, 2023
- * Updated on Oct 28, 2023
+ * Created on Oct 28, 2023
+ * Updated on Oct 30, 2023
  *
  * Description: .
  * Dependenties: js/config.js
@@ -19,8 +19,8 @@
 /*
  * Function:    loadSheet
  *
- * Created on Oct 02, 2023
- * Updated on Oct 27, 2023
+ * Created on Oct 28, 2023
+ * Updated on Oct 30, 2023
  *
  * Description: The sheet.html main function.
  *
@@ -29,72 +29,112 @@
  *
  */
 function loadSheet() {
+
+    // Get href anchor from URL.
+    var anchor = window.location.hash.substring(1);
+           
+    // Show title and current year.
+    var i = showPageTitle(anchor);
     
-    // Show current year in header
-    $("header h1 span").html(cDate.getFullYear());
+    addYearPicker();
+    
+    // Fill the slide menu.
+    fillSheetSlideMenu(cDate.getMonth(), -1);
+    
+    // Change slide menu scale (months, quarters, year).
+    $("#sheet_buttons").on('click', 'img', function () {
+	changeSlideMenuScale(this);
+    });	
+    
+
+    // Temp. solution. Will be fixed when bar scale is red from the database.
+    $("#slide6").hide();
+    $(".slider .bar").css("width", "8.33%");
+    $(".slidemenu label").css("width", "8.33%");
+  
+    
+    // Fill hamburger menu.
+    fillHamburgerMenu(cMenu[i]);
+
+    // Process menu selection and reload the page.
+    $(".menu_item").click(function () {
+        window.location.href=this;
+        window.location.reload(true);
+    });
+    
+    showPageTheme(anchor);
    
-    // YearPicker Test
+    
+    
+    // Fade in the page.
+    $("html").fadeIn("slow");
+}
+
+/*
+ * Function:    showPageTitle
+ *
+ * Created on Oct 28, 2023
+ * Updated on Oct 28, 2023
+ *
+ * Description: Show the title of the page.
+ *
+ * In:  page
+ * Out: i
+ *
+ */
+function showPageTitle(page) {
+    
+    var i;
+    switch (page) {
+        case "finance" : i = 1; 
+            break;
+        case "stock"   : i = 2;
+            break;
+        case "savings" : i = 3;
+            break;
+        case "crypto"  : i = 4;
+            break;
+    }
+    
+    $("header h1").html(cMenu[i] + " <span>" + cDate.getFullYear() + "</span>");
+    
+    return i;
+}
+
+/*
+ * Function:    addYearPicker()
+ *
+ * Created on Oct 30, 2023
+ * Updated on Oct 30, 2023
+ *
+ * Description: Add the YearPicker popup box.
+ *
+ * In:  -
+ * Out: -
+ *
+ */
+function addYearPicker() {
+    
     $(".yearpicker").yearpicker({
 	year: cDate.getFullYear(),
 	startYear: 1998,
         endYear: cDate.getFullYear(),
             onHide:function(value)
             {
-                $("header h1 span").html(value);
-			
-		let i = $(".slidemenu input[name='slideItem']:checked").val();
-                                
-                fillSlideMenu(i, -1);
+                $("header h1 span").html(value);			
+		let i = $(".slidemenu input[name='slideItem']:checked").val();                               
+                fillSheetSlideMenu(i, -1);
                 
-                $("#tst_text").html("<h1>" + cMonths[i] + " " + value +"</h1>");
+                //$("#tst_text").html("<h1>" + cMonths[i] + " " + value +"</h1>");
             }
-    });
-  
-
-    // Fill the slide menu.
-    fillSlideMenu(cDate.getMonth(), -1);
-
-    // Change slide menu scale (months, quarters, year).
-    $("#sheet_buttons").on('click', 'img', function () {
-	changeSlideMenuScale(this);
-    });	
-	
-	
-/*	
-    // Setting popup.
-    $("#sheet_buttons").on('click', function () {
-        $(".popup_content").show();
-    });
-*/
-    
-    // TEST
-    $("#tst_text").html("<h1>" + cMonths[cDate.getMonth()] + " " + cDate.getFullYear() + "</h1>");
-	
-    // TEST: Slide menu item clicked
-    $(".slidemenu input[name='slideItem']").change(function() {
-        
-        let i = $(".slidemenu input[name='slideItem']:checked").val();
-        let year = $("header h1 span").html();
-        
-        // TEST
-        $("#tst_text").html("<h1>" + cMonths[i] + " " + year + "</h1>");
-        
-        //console.log("Test click:" + i);
     });    
-    
-    
-    //getConstants();
-    
-    // Debug
-    //console.log("Test: " + cDate.getFullYear());
 }
 
-
 /*
- * Function:    fillSlideMenu
+ * Function:    fillSheetSlideMenu
  *
  * Created on Oct 21, 2023
- * Updated on Oct 28, 2023
+ * Updated on Oct 30, 2023
  *
  * Description: Fill the Slidemenu bar with the items.
  *
@@ -102,7 +142,7 @@ function loadSheet() {
  * Out: -
  *
  */
-function fillSlideMenu(active, quarter) {
+function fillSheetSlideMenu(active, quarter) {
 		
     var max, hide, check = false;
     var year = $("header h1 span").html();
@@ -111,7 +151,7 @@ function fillSlideMenu(active, quarter) {
     var scale = $("#sheet_buttons img:first-child").attr("alt");
 	
     switch(scale) {
-        case "months" :
+        case "quarters" :
        	max = 12;
 	items = cMonths;
 			
@@ -124,7 +164,7 @@ function fillSlideMenu(active, quarter) {
 	}			
 	break;
 			
-        case "quarters" :
+        case "year" :
             max = 6;
             items = cQuarters;
 					
@@ -146,7 +186,7 @@ function fillSlideMenu(active, quarter) {
             check = true;
             break;
 			
-	case "year" :
+	case "months" :
             max = 6;
             items = cYear;	
             hide = 0;
@@ -177,7 +217,7 @@ function fillSlideMenu(active, quarter) {
  * Function:    changeSlideMenuScale
  *
  * Created on Oct 25, 2023
- * Updated on Oct 27, 2023
+ * Updated on Oct 30, 2023
  *
  * Description: Change the Slide menu scale (months, quarters, year).
  *
@@ -188,7 +228,7 @@ function fillSlideMenu(active, quarter) {
  function changeSlideMenuScale(that) {
 	
     switch(that.alt) {
-	case "months" : 
+	case "quarters" : 
             // Change month to quarter.
             var month = $(".slidemenu input[name='slideItem']:checked").val();
             var quarter = Math.ceil((Number(month) + 1) / 3) - 1;
@@ -197,29 +237,59 @@ function fillSlideMenu(active, quarter) {
             $(".slider .bar").css("width", "16.66%");
             $(".slidemenu label").css("width", "16.66%");
             $("#slide6").show();
-            $(that).attr("src","img/quarters.png");
-            $(that).attr("alt","quarters");
+            $(that).attr("src","img/year.png");
+            $(that).attr("alt","year");
 	
             // The quarter varible is only use when the scale changes from months to quarters.
-            fillSlideMenu(3, quarter);
-            break;
-			
-	case "quarters" :
-            $(that).attr("src","img/year.png");
-            $(that).attr("alt","year");		
-			
-            fillSlideMenu(0, -1);
+            fillSheetSlideMenu(3, quarter);
             break;
 			
 	case "year" :
+            $(that).attr("src","img/months.png");
+            $(that).attr("alt","months");		
+			
+            fillSheetSlideMenu(0, -1);
+            break;
+			
+	case "months" :
             $("#slide6").hide();
             $(".slider .bar").css("width", "8.33%");
             $(".slidemenu label").css("width", "8.33%");
             $("#slide12").show();
-            $(that).attr("src","img/months.png");
-            $(that).attr("alt","months");
+            $(that).attr("src","img/quarters.png");
+            $(that).attr("alt","quarters");
 				
-            fillSlideMenu(0, -1);
+            fillSheetSlideMenu(0, -1);
             break;
     }
+}
+
+/*
+ * Function:    showPageTheme
+ *
+ * Created on Oct 29, 2023
+ * Updated on Oct 30, 2023
+ *
+ * Description: Show the sheet page theme colors.
+ *
+ * In:  page
+ * Out: -
+ *
+ */
+function showPageTheme(page) {
+    
+    switch (page) {
+        case "finance" : $(":root").css("--selected-text-color", "#ffd700"); // #ffe87c
+                         $(".slider .bar").css("background","#ffd700");            
+                         break;
+        case "stock"   : $(":root").css("--selected-text-color", "#228b22"); 
+                         $(".slider .bar").css("background","#228b22");
+                         break;
+        case "savings" : $(":root").css("--selected-text-color", "#4169e1 "); 
+                         $(".slider .bar").css("background","#4169e1");
+                         break;
+        case "crypto"  : $(":root").css("--selected-text-color", "#ff8f00"); 
+                         $(".slider .bar").css("background","#ff8f00");
+                         break;
+    }  
 }
